63f5f8d0bda838d2ac772d86783558529bc08354
MassimoRomano <romano93m@gmail.com>
2024-03-04 17:55:09 +0100

    add structure for card and finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,5 +33,24 @@ const team = [
     },
   ];
 
-
+  // Otteniamo il riferimento al contenitore
   const teamContainer = document.getElementById('team-container');
+  // Creiamo una card per ogni membro del team
+  team.forEach(member => {
+    // Creazione del contenitore della card
+    const memberCard = document.createElement('div');
+    // Aggiungiamo delle classi per la griglia con Bootstrap
+    memberCard.classList.add('col-lg-4', 'col-md-6', 'member-card');
+    //Costruiamo l'hmtl interno delle card
+    memberCard.innerHTML = `
+      <div class="card bg-info text-center ">
+        <img class="card-img-top " src="./assets/img/${member.image}">
+        <div class="card-body">
+          <h5 class="card-title">${member.name}</h5>
+          <p class="card-text">${member.role}</p>
+        </div>
+      </div>
+    `;
+    // Aggiunta della card al contenitore 
+    teamContainer.appendChild(memberCard);
+  });
